5593d2aa9379e8afc434b9b67fa6a4a0776ac950
Luis Carlos Contreras <jinchuika@gmail.com>
2017-03-16 09:47:47 -0600

    Comentarios anidados

--- app/models.py
+++ app/models.py
@@ -5,9 +5,16 @@ class Post(models.Model):
     titulo = models.CharField(max_length=20)
     cuerpo = models.TextField()
 
+    def __str__(self):
+        return self.titulo
+
 
 class Comentario(models.Model):
     post = models.ForeignKey(Post, related_name='comentarios')
+    padre = models.ForeignKey("self", related_name='hijos', null=True, blank=True)
     fecha = models.DateField()
     usuario = models.CharField(max_length=50)
     mensaje = models.TextField()
+
+    def __str__(self):
+        return str(self.fecha) + self.mensaje[:10]
